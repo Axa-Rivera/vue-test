595b30c86f76b67502044f12c46a8b22cf4b7b54
vgarcia72 <vgarciag@hotmail.com>
2023-12-14 23:07:06 -0600

    Formateo de condiciones, eventos y css

--- src/views/ConditionalsView.vue
+++ src/views/ConditionalsView.vue
@@ -1,24 +1,30 @@
-<template lang="">
-  <div>
-    <h2>V-If</h2>
-    <label>
-      <input type="checkbox" id="checkbox-vif" title="checkBox" v-model="checked" />
-      {{ checked ? 'True' : 'False' }}
-    </label>
-    <div v-if="checked">
-      <p>Ahora me puedes ver</p>
-      <button type="button">Button If</button>
+<template>
+  <div class="row">
+    <div class="col s6">
+      <h2>V-If</h2>
+      <label>
+        <input type="checkbox" class="filled-in" v-model="checked" id="checkbox-vif" />
+        <span>{{ checked ? 'True' : 'False' }}</span>
+      </label>
+      <div v-if="checked">
+        <p>Ahora me puedes ver</p>
+        <a class="waves-effect waves-light btn-small">Button If</a>
+      </div>
+      <p v-else>Yo soy el else</p>
+      <label>Value: {{ checked }}</label>
+      <p>
+        <a class="waves-effect waves-light btn-small" @click="validateChecked">Button Checked</a>
+      </p>
     </div>
-    <p v-else>Yo soy el else</p>
-    <label>Value: {{ checked }}</label>
-    <p><button type="button" @click="validateChecked">Button Checked</button></p>
 
-    <h2>V-Show</h2>
-    <label>
-      <input type="checkbox" id="checkbox-vshow" title="checkedShow" v-model="checkedShow" />
-      {{ checkedShow ? 'True' : 'False' }}
-    </label>
-    <p v-show="checkedShow">Ahora me puedes ver</p>
+    <div class="col s6">
+      <h2>V-Show</h2>
+      <label>
+        <input type="checkbox" id="checkbox-vshow" v-model="checkedShow" />
+        <span>{{ checkedShow ? 'True' : 'False' }}</span>
+      </label>
+      <p v-show="checkedShow">Ahora me puedes ver</p>
+    </div>
   </div>
 </template>
 <script>
